40e77475404d581e8c80f9d753c96687280b414a
semerson4 <ssollars@nd.edu>
2026-09-03 10:15:39 -0400

    Update week_2

--- Week_2/week_2.py
+++ Week_2/week_2.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 
 print("Successful setup:")
-print(f"panda versions:{pd.__.version__}")
+print(f"panda versions:{pd.__version__}")
 print(f"numpy versions:{np.__version__}")
