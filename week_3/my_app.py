156480c3a6a881f46eb7bee4f75e33dec6d495b7
semerson4 <ssollars@nd.edu>
2026-09-15 10:13:44 -0400

    streamlit-edits

--- week_3/my_app.py
+++ week_3/my_app.py
@@ -7,3 +7,20 @@ if st.button('Click me'):
     st.write('Button clicked!')
 else:
     st.write('Button not clicked yet.')
+
+
+import pandas as pd
+
+st.subheader("Exploring Our Dataset")
+df = pd.read_csv("data/sample_data.csv")
+
+# if in week_2 or somewhere else, still do pd.read.csv("") but use "./Week_2/sample_data.csv"
+
+st.write("Here's our data")
+st.dataframe(df)
+
+city = st.selectbox("Select a city", df["City"].unique())
+st.write(f"People in {city}")
+st.dataframe(df[df["City"]== city])
+
+st.bar_chart(df["Salary"])
